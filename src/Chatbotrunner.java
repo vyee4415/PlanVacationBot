a9a109b688b9291941e86c88156e91d2b767e9d5
BT_1N3_07 <BT_1N3_07@BT_1N3_07.bktech.local>
2017-10-24 09:11:35 -0400

    ;

---
+++ src/Chatbotrunner.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+/**
+ * A simple class to run our chatbot teams.
+ * @author Mr. Levin
+ * @version September 2017
+ */
+public class Chatbotrunner
+{
+
+	/**
+	 * Create instances of each chatbot, give it user input, and print its replies. Switch chatbot responses based on which chatbot the user is speaking too.
+	 */
+	public static void main(String[] args)
+	{
+		ChatBotLevin chatbot1 = new ChatBotLevin();
+		
+		System.out.println (chatbot1.getGreeting());
+		Scanner in = new Scanner (System.in);
+		String statement = in.nextLine();
+		
+
+
+		while (!statement.equals("Bye"))
+		{
+			System.out.println (chatbot1.getResponse(statement));
+			statement = in.nextLine();
+		}
+	}
+
+}
